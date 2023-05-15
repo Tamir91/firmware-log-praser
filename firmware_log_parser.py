99ebec92a73b3072b22d9177d63207e7cad611ab
Tamir <1hotlev@gmail.com>
2023-05-15 18:24:00 +0300

    some functions added

--- firmware_log_parser.py
+++ firmware_log_parser.py
@@ -1,16 +1,89 @@
-# This is a sample Python script.
+"""
+This script parse firmware log
+Usage
+"""
+import getopt
+import os.path
+import re
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# from cgi import log
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def usage() -> None:
+    """
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    :return:
+    :rtype:
+    """
+    pass
+
+
+#  1. open .log
+def remove_characters(file_link: str) -> str:
+    """
+
+    :param file_link:
+    :type file_link:
+    :return:
+    :rtype:
+    """
+    if not os.path.isfile(file_link):
+        usage()
+        exit(1)
+
+    with open(file_link, 'r') as file:
+        data = file.read()
+
+        data.replace(' ', '')  # remove white spaces
+        data = re.sub("[^0-9,]", "", data)  # remove all except numbers and ','
+
+    return data
+
+def split_string(string: str, separator: int) -> str:
+    if str and type(string) is str and type(separator) is int:
+        mat = []
+
+        for (i : str)
+
+
+
+
+
+#  3. create matrix of lines
+
+
+# Entry point
+required_tags = []
+opts = []
+args = ""
+
+# parse command-line:
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "fx", longopts=['lof-file', 'xml-events'])
+except getopt.GetoptError as err:
+    # log.e(err)
+    print("Error in get opt")
+    usage()
+
+for opt, arg in opts:
+    if opt in ('-h', '--help'):
+        usage()
+    elif opt in ('-f', '--lof-file'):
+        pass
+    elif opt in ('-x', '--xml-events'):
+        pass
+
+if len(args) != 1:
+    usage()
+
+#  remove_words(arg...)
+logs = remove_characters("d457_fw_lose_vc.log")
+list = logs.split(',')
+
+lines = numpy.array_split(list, 160)
+print(lines)
+
